9662192704fab0f736e1268eae5ef7c3fb0ff3a4
hui00 <kevinbrinkschulte@hotmail.de>
2023-01-13 23:39:42 +0100

    add validation and controller crud

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,50 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "Kitten saved"
+      redirect_to kittens_path
+    else
+      flash[:error] = "Kitten not saved"
+      render "new", status: :unprocessable_entity
+    end
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      flash[:success] = "Kitten updated"
+      redirect_to @kitten
+    else
+      flash[:error] = "Kitten not updated"
+      render "edit", status: :unprocessable_entity
+    end
   end
 
-  def destory
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:success] = "Kitten deleted"
+    redirect_to root_path, status: :see_other
+  end
+
+  private def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -11,4 +11,8 @@
 #  updated_at :datetime         not null
 #
 class Kitten < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :age, presence: true
+  validates :cuteness, presence: true
+  validates :softness, presence: true
 end
